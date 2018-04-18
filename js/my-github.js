fa06cc4e8914ba535332f11f82fadd0934ef9bd4
Awet Fessha <awetkifle94@gmail.com>
2018-04-18 22:24:27 +0100

    Awet: Completed Dom-ajax exercises

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,45 @@
 // Write code here to communicate with Github
+var searchBtn = document.querySelector("#searchBtn");
+var search = document.querySelector("#search");
+var repoList = document.querySelector("#repos-list");
+function fetchUser(userName) {
+  repoList.innerHTML = "";
+  var url = "https://api.github.com/users/" + userName + "/repos";
 
+  var reposCount = document.querySelector("#repos-count");
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(repos) {
+      repos.forEach(function(repo) {
+        var a = document.createElement("a");
+        a.setAttribute("href", repo.html_url);
+        a.innerHTML = repo.name;
+        // var linkText = document.createTextNode(repo.name);
+        // a.appendChild(linkText);
+
+        var listElements = document.createElement("li");
+        listElements.appendChild(a);
+        repoList.appendChild(listElements);
+      });
+      reposCount.innerHTML = repos.length;
+    });
+}
+fetchUser("akfessha");
+
+searchBtn.addEventListener("click", function() {
+  var userName = "akfessha";
+  if (search.value.length > 0) {
+    var userName = search.value;
+  }
+  fetchUser(userName);
+});
+
+/* fetch("https://jobs.github.com/positions.json", { mode: "no-cors" })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(message) {
+    console.log(message);
+  }); */
---
+++ js/pull-requests.js
@@ -0,0 +1,56 @@
+var pullRequestList = document.querySelector("#pull-requests-list");
+function outputList(resultsArray) {
+  console.log(resultsArray);
+  resultsArray.forEach(function(pullRequest) {
+    var a = document.createElement("a");
+    a.setAttribute("href", pullRequest.html_url);
+    var linkText = document.createTextNode(pullRequest.title);
+    a.appendChild(linkText);
+
+    var listElements = document.createElement("li");
+    listElements.appendChild(a);
+    pullRequestList.appendChild(listElements);
+  });
+}
+fetch("https://api.github.com/repos/codeyourfuture/js-exercises/pulls")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(pullRequests) {
+    var resultsArray = [];
+    pullRequests.forEach(function listingPullRequest(pullRequest) {
+      //  if (pullRequest.user.id === 31638686) {
+      /*
+     
+      */
+      resultsArray.push(pullRequest);
+
+      //  }
+    });
+    outputList(resultsArray);
+    var mySearch = document.querySelector("#mySearch");
+
+    mySearch.addEventListener("keyup", function(event) {
+      const value = event.target.value;
+      pullRequestList.innerHTML = "";
+      pullRequests.forEach(function(pullRequest) {
+        console.log(pullRequest.user.login);
+        console.log(value);
+        console.log(pullRequest.user.login.search(value));
+
+        if (
+          pullRequest.user.login.toLowerCase().search(value.toLowerCase()) !==
+          -1
+        ) {
+          var a = document.createElement("a");
+          a.setAttribute("href", pullRequest.html_url);
+          var linkText = document.createTextNode(pullRequest.title);
+          a.appendChild(linkText);
+
+          var listElements = document.createElement("li");
+          listElements.appendChild(a);
+          pullRequestList.appendChild(listElements);
+        }
+      });
+    });
+  });
