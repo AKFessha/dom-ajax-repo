5217b0e70cd89bdf2d4aa4945fc26e4dd6547208
Awet Fessha <awetkifle94@gmail.com>
2018-05-03 11:45:11 +0100

    changed some of the functions to arrow function

--- js/others.js
+++ js/others.js
@@ -1,8 +1,6 @@
 var jokeContainer = document.querySelector("#api-result");
 fetch("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten")
-  .then(function(response) {
-    return response.json();
-  })
+  .then(response => response.json())
   .then(function(jokes) {
     jokes.forEach(joke => {
       // console.log(joke.setup + ": " + joke.punchline);
